9b685167297afc54a157381fd6c7269d21984ac2
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-24 15:09:00 -0300

    models

--- training-net/src/Controllers/MoviesController.cs
+++ training-net/src/Controllers/MoviesController.cs
@@ -5,13 +5,21 @@ namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
+        // 
+        // GET: /HelloWorld/    
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Welcome(string name)
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
             ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
             return View();
         }
     }
