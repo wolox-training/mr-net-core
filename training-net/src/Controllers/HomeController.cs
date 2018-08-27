60def89c217160196fd351e9f7fe0e6a2712d411
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-27 15:30:48 -0300

    controllers 4

--- training-net/src/Controllers/HomeController.cs
+++ training-net/src/Controllers/HomeController.cs
@@ -17,12 +17,12 @@ namespace src.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-             return View();
+            return View();
         }
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-             return View();
+            return View();
         }
         public IActionResult Privacy()
         {
