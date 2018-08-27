79376bebe845ea1db58280ebab5c2c088d9a3cfc
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-27 14:43:44 -0300

    controllers 3

--- training-net/src/Controllers/HomeController.cs
+++ training-net/src/Controllers/HomeController.cs
@@ -14,21 +14,21 @@ namespace src.Controllers
         {
             return View();
         }
-         public IActionResult About()
+        public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
              return View();
         }
-         public IActionResult Contact()
+        public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
              return View();
         }
-         public IActionResult Privacy()
+        public IActionResult Privacy()
         {
             return View();
         }
-         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
