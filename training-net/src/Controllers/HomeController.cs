3a40632cd9f383d89de8d0f0255991b75cd8ed74
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-27 14:31:51 -0300

    controllers 2

--- training-net/src/Controllers/HomeController.cs
+++ training-net/src/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,25 +14,21 @@ namespace src.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
+         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-            return View();
+             return View();
         }
-
-        public IActionResult Contact()
+         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-            return View();
+             return View();
         }
-
-        public IActionResult Privacy()
+         public IActionResult Privacy()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
