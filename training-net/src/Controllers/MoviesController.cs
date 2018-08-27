f1a6c2e79c6d557e6f89b984d54ee52d54fcd414
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-27 12:45:55 -0300

    internalization

--- training-net/src/Controllers/MoviesController.cs
+++ training-net/src/Controllers/MoviesController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly IHtmlLocalizer<MoviesController> _localizer;
+        
+        public MoviesController(IHtmlLocalizer<MoviesController> localizer)
+        {
+            this._localizer = localizer;
+        }
         // 
         // GET: /HelloWorld/    
 
