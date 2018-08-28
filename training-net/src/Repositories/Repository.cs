3564e2ce07a7fa934ade4e755a1dbd44b837de39
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-28 14:57:08 -0300

    repositories

--- training-net/src/Repositories/Repository.cs
+++ training-net/src/Repositories/Repository.cs
@@ -9,7 +9,6 @@ namespace Repositories
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        //posible problema
         protected readonly DbContext Context;
 
         public Repository(DbContext context)
@@ -19,24 +18,11 @@ namespace Repositories
 
         public TEntity Get(int id)
         {
-            // Here we are working with a DbContext, not PlutoContext. So we don't have DbSets 
-            // such as Courses or Authors, and we need to use the generic Set() method to access them.
             return Context.Set<TEntity>().Find(id);
         }
 
         public IEnumerable<TEntity> GetAll()
         {
-            // Note that here I've repeated Context.Set<TEntity>() in every method and this is causing
-            // too much noise. I could get a reference to the DbSet returned from this method in the 
-            // constructor and store it in a private field like _entities. This way, the implementation
-            // of our methods would be cleaner:
-            // 
-            // _entities.ToList();
-            // _entities.Where();
-            // _entities.SingleOrDefault();
-            // 
-            // I didn't change it because I wanted the code to look like the videos. But feel free to change
-            // this on your own.
             return Context.Set<TEntity>().ToList();
         }
 
