f4bbdf53a7aaa2f80af65551c097f80257b947d4
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-29 15:20:18 -0300

    repositories 2

--- training-net/src/Repositories/Interfaces/IRepository.cs
+++ training-net/src/Repositories/Interfaces/IRepository.cs
@@ -10,10 +10,8 @@ namespace Repositories.Interfaces
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
         TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
-
         void Add(TEntity entity);
         void AddRange(IEnumerable<TEntity> entities);
-        
         void Remove(TEntity entity);
         void RemoveRange(IEnumerable<TEntity> entities);
     }
