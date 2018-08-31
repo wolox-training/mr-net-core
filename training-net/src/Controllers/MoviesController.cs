ea90f3a218c0b734f45bb63cc5252b7c37dfd88e
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-08-31 12:19:47 -0300

    edit

--- training-net/src/Controllers/MoviesController.cs
+++ training-net/src/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
 using MvcMovies.Models.Views;
 using Repositories.Interfaces;
@@ -57,9 +58,23 @@ namespace MvcMovie.Controllers
         [HttpPost]
         public IActionResult Edit(MovieViewModel mvm)
         {   
+            try{
             UnitOfWork.Movies.Update(new Movie {ID=mvm.ID,Title=mvm.Title,ReleaseDate=mvm.ReleaseDate,Genre=mvm.Genre,Price=mvm.Price});
             UnitOfWork.Complete();
-            return View();
+            
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (mvm == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
