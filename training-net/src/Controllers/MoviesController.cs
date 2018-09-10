8445fea95a774077cad4f3a31588b340cc4d90e9
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-09-10 15:45:31 -0300

    order 2

--- training-net/src/Controllers/MoviesController.cs
+++ training-net/src/Controllers/MoviesController.cs
@@ -190,31 +190,5 @@ namespace MvcMovie.Controllers
             return RedirectToAction("Index", "Movies");
         }
 
-        [HttpGet("Mail")]
-        public IActionResult Mail(int id)
-        {
-            var movie = UnitOfWork.MovieRepository.Get(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(new MovieViewModel { ID = movie.ID, Title = movie.Title, ReleaseDate = movie.ReleaseDate,Genre = movie.Genre, Price = movie.Price });  
-        }
-
-        [HttpPost("SendMail")]
-        public IActionResult SendMail(MovieViewModel mvm)
-        {
-            var client = new SmtpClient("smtp.mailtrap.io", 2525);
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("12b024d9d9d9ac", "bfcaf23d60563e");
-            client.EnableSsl = true;
-            var mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("whoever@me.com");
-            mailMessage.To.Add("receiver@me.com");
-            mailMessage.Body = string.Format("Title: {0} \n Release Date: {1} \n Genre: {2} \n Price {3}".Replace("\n",Environment.NewLine), mvm.Title, mvm.ReleaseDate.ToString(), mvm.Genre, mvm.Price.ToString());
-            mailMessage.Subject = "subject";
-            client.Send(mailMessage);
-            return RedirectToAction("Index", "Movies");
-        }
     }
 }
