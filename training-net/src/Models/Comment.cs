1c0eb2c7bd4bcd777ccd9494b7b7de46e92f2e11
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-09-11 16:50:08 -0300

    comments view 3

--- training-net/src/Models/Comment.cs
+++ training-net/src/Models/Comment.cs
@@ -15,7 +15,11 @@ namespace MvcMovie.Models
         public DateTime Date { get; set; }
 
         public string Rating { get; set; }
+<<<<<<< 2e340beca05f69e4ef0c3d5b0dd2475807719245
         
         public Movie Movie { get; set; }
+=======
+        public int MovieID { get; set; }
+>>>>>>> comments view 3
     }
 }
