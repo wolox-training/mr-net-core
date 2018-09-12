05117e3801367ffc92ab89040aa56cf28ded64fd
Marcosnr97 <marcos.rios@wolox.com.ar>
2018-09-12 14:49:55 -0300

    comments view 5

--- training-net/src/Models/Comment.cs
+++ training-net/src/Models/Comment.cs
@@ -14,12 +14,7 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
-        public string Rating { get; set; }
-<<<<<<< 2e340beca05f69e4ef0c3d5b0dd2475807719245
-        
+        public string Rating { get; set; }  
         public Movie Movie { get; set; }
-=======
-        public int MovieID { get; set; }
->>>>>>> comments view 3
     }
 }
--- training-net/src/Repositories/CommentRepository.cs
+++ training-net/src/Repositories/CommentRepository.cs
@@ -16,5 +16,10 @@ namespace Repositories
         }
 
         public DbContext DbContext{ get { return Context as DbContext; } }
+
+        // IEnumerable<Comment> ICommentRepository.GetMovieComments(int id)
+        // {
+        //     return Context.Set<Comment>().Where(m => m.mo).ToList();
+        // }
     }
 }
