decf4c70eb4433ca70169ef03459f5d2f42d0557
Marcosnr97 <marcos.rios@wolox.com.ar>
2019-08-26 16:38:48 -0300

    Added login view and login integration with api

--- wolox-books/src/services/AuthService.js
+++ wolox-books/src/services/AuthService.js
@@ -1,3 +1,5 @@
 import { api } from '../config/api'
 
 export const Register = userDetails => api.post('/users', userDetails)
+
+export const Login = credentials => api.post('users/sessions', credentials)
